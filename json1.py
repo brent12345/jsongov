73e8dd2e8b6e377e2382bcabb95bdfe9e18e9ad5
brent.morine <brent.morine@WIR243-5450-NS>
2018-01-03 09:02:58 -0400

    testing

--- json1.py
+++ json1.py
@@ -1,31 +1,30 @@
 #!/usr/local/bin/python
 # coding=utf-8
 """
- Simple example of a Python script used to query an API.
-
- @note:    This is a simple example without any error handling.
- @license: http://data.gc.ca/eng/open-government-licence-canada
+script imports Federal projects in Canada
 """
 import json
-
 import re
 import requests
 import urllib.request
 
-url = "http://www.infrastructure.gc.ca/alt-format/opendata/project-list-liste-de-projets-bil.json"
+url = "https://data.novascotia.ca/api/views/7dfb-h4nw/rows.json?accessType=DOWNLOAD"
 
 with urllib.request.urlopen(url) as url:
     data = json.loads(url.read().decode())
     #print(data['data'][0], data['data'][1])
 
-for x in data['data']:
-    print(x[7], x[9])
+
 
 for d in data['data']:
-    str1 = d[1]
-    match = re.search(r'nova', str1)
+    str1 = d[12]
+    #match = re.search(r'nova', str1)
     # If-statement after search() tests if it succeeded
-    if match:
-        print('found', match.group()) ## 'found word:cat'
-    else:
-        print('did not find')
+    #if match:
+        #for x in data['data']:
+           # print(x[7], x[9])
+             #print(match.string)
+    #else:
+            #pass
+    if int(str1) > 12:
+        print('found')
